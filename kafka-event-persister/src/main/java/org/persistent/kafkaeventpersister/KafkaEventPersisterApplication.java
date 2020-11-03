634d895fa8b15a24b4de4550d057faa54e4a8984
KHemamal <khemamal@innosoftwareworld.com>
2020-11-03 21:28:46 +0530

    Added the kafka event persister.

---
+++ kafka-event-persister/src/main/java/org/persistent/kafkaeventpersister/KafkaEventPersisterApplication.java
@@ -0,0 +1,13 @@
+package org.persistent.kafkaeventpersister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEventPersisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaEventPersisterApplication.class, args);
+	}
+
+}
---
+++ kafka-event-persister/src/test/java/org/persistent/kafkaeventpersister/KafkaEventPersisterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.persistent.kafkaeventpersister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEventPersisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
